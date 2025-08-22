ffe0649b4ccf19611b9ea793a5e1ec416355dcdf
jinstomy <jinstomy7686@gmail.com>
2025-08-22 16:00:28 +0530

    Update CartStepsDefinition.java

--- src/main/java/stepdefenition/CartStepsDefinition.java
+++ src/main/java/stepdefenition/CartStepsDefinition.java
@@ -14,29 +14,26 @@ public class CartStepsDefinition {
 
     @And("I add two products to the cart")
     public void iAddTwoProductsToTheCart() {
-        // Write code here that turns the phrase above into concrete actions
         cartPage.addToCart();
-       // throw new PendingException();
+    
     }
 
     @And("I should be on the checkout information page")
     public void iShouldBeOnTheCheckoutInformationPage() {
         // Write code here that turns the phrase above into concrete actions
         cartPage.checkout();
-        //throw new PendingException();
     }
 
     @Then("User fills in checkout information with {string}, {string}, and {string}")
     public void userFillsInCheckoutInformationWithAnd(String finame, String lsname, String pcode) {
         cartPage.checkoutInfo(finame, lsname, pcode);
-       // throw new PendingException();
     }
 
     @And("User completes the purchase")
     public void userCompletesThePurchase() {
         // Write code here that turns the phrase above into concrete actions
         cartPage.finish();
-        //throw new PendingException();
+    
     }
 
     @Then("User should see the {string} page")
@@ -45,6 +42,5 @@ public class CartStepsDefinition {
 
         CartPage completePage = new CartPage(driver);
         Assert.assertTrue("Thank You message not displayed", completePage.isThankYouMessageDisplayed());
-        //throw new PendingException();
     }
 }
